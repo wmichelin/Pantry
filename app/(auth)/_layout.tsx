07d65eb3b9bec004424f852ec637bd7d25f9e75b
Walter Michelin <wmichelin@gmail.com>
2026-03-31 21:46:42 -0400

    redirect after login

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,7 +1,21 @@
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
+import { useAuth } from "../../lib/auth-context";
+import { ActivityIndicator, View } from "react-native";
 
 export default function AuthLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }} />
-  );
+  const { session, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  if (session) {
+    return <Redirect href="/(app)" />;
+  }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
